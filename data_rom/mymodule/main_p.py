c9d1e0e599022d8a7fe75334a168ada9d951151e
coob <koosanghwi@gmail.com>
2024-02-28 20:38:50 +0900

    ver 1.00

--- data_rom/mymodule/main_p.py
+++ data_rom/mymodule/main_p.py
@@ -139,7 +139,7 @@ class MyApp(QDialog):
         # pyinstaller --hidden-import PyQt5 --hidden-import pyserial --hidden-import requests --hidden-import chardet --add-data="C:\\my_games\\rom\\data_rom;./data_rom" --name rom -i="rom_macro.ico" --add-data="rom_macro.ico;./" --icon="rom_macro.ico" --paths "C:\Users\1_S_3\AppData\Local\Programs\Python\Python311\Lib\site-packages\cv2" main.py
 
         # self.setGeometry(1000 + 960 + 960, 300, 900, 600)
-        self.setGeometry(20, 200, 900, 700)
+        self.setGeometry(20 + 960, 200, 900, 700)
         self.show()
     def my_title(self):
         self.setWindowTitle(v_.this_game + "(ver " + version + ")")
