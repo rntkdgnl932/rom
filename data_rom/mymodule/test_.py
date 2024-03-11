652826467ca48d291548411927bcd596ef6d1e31
coob <koosanghwi@gmail.com>
2024-03-11 09:01:04 +0900

    ver 1.741

--- data_rom/mymodule/test_.py
+++ data_rom/mymodule/test_.py
@@ -52,13 +52,37 @@ def go_test():
     # data = "던전_일반_카타콤_1"
     # dungeon_start(cla, data)
 
-    full_path = "c:\\my_games\\rom\\data_rom\\imgs\\logout\\kakao_login_btn.png"
+    # full_path = "c:\\my_games\\rom\\data_rom\\imgs\\logout\\kakao_login_btn.png"
+    # img_array = np.fromfile(full_path, np.uint8)
+    # img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    # imgs_ = imgs_set_(280, 150, 680, 450, "two", img, 0.7)
+    # if imgs_ is not None and imgs_ != False:
+    #
+    #     print("kakao_login_btn", imgs_)
+    #     click_pos_reg(imgs_.x, imgs_.y, cla)
+    #     for i in range(10):
+    #         full_path = "c:\\my_games\\rom\\data_rom\\imgs\\logout\\auto_login_btn.PNG"
+    #         img_array = np.fromfile(full_path, np.uint8)
+    #         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    #         imgs_ = imgs_set_(280, 150, 680, 450, "two", img, 0.7)
+    #         if imgs_ is not None and imgs_ != False:
+    #             print("auto_login_btn", imgs_)
+    #             full_path = "c:\\my_games\\rom\\data_rom\\imgs\\logout\\auto_login_confirm.PNG"
+    #             img_array = np.fromfile(full_path, np.uint8)
+    #             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    #             imgs_ = imgs_set_(280, 150, 680, 450, "two", img, 0.7)
+    #             if imgs_ is not None and imgs_ != False:
+    #                 print("auto_login_confirm", imgs_)
+    #                 click_pos_reg(imgs_.x, imgs_.y, cla)
+    #                 break
+    #         time.sleep(0.5)
+
+    full_path = "c:\\my_games\\rom\\data_rom\\imgs\\logout\\kakao_login_click_btn.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(110, 130, 700, 700, "two", img, 0.7)
+    imgs_ = imgs_set_(280, 350, 680, 450, "two", img, 0.7)
     if imgs_ is not None and imgs_ != False:
-
-        print("kakao_login_btn", imgs_)
+        print("kakao_login_click_btn", imgs_)
 
 
     # full_path = "c:\\my_games\\rom\\data_rom\\imgs\\potion\\full_potion.PNG"
