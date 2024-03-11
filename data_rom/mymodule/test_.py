85e4dcbfed3e7656ce0e90ed98d3f3b3ff950149
coob <koosanghwi@gmail.com>
2024-03-11 09:06:29 +0900

    ver 1.741

--- data_rom/mymodule/test_.py
+++ data_rom/mymodule/test_.py
@@ -80,10 +80,20 @@ def go_test():
     full_path = "c:\\my_games\\rom\\data_rom\\imgs\\logout\\kakao_login_click_btn.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(280, 350, 680, 450, "two", img, 0.7)
+    imgs_ = imgs_set_(280, 350, 1900, 900, "one", img, 0.7)
     if imgs_ is not None and imgs_ != False:
         print("kakao_login_click_btn", imgs_)
 
+        # id => y - 170
+
+    full_path = "c:\\my_games\\rom\\data_rom\\imgs\\logout\\ganpyun_login.PNG"
+    img_array = np.fromfile(full_path, np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    imgs_ = imgs_set_(280, 350, 1900, 900, "one", img, 0.7)
+    if imgs_ is not None and imgs_ != False:
+        print("ganpyun_login", imgs_)
+
+        # id => y - 170
 
     # full_path = "c:\\my_games\\rom\\data_rom\\imgs\\potion\\full_potion.PNG"
     # img_array = np.fromfile(full_path, np.uint8)
