638a85cf9122abc5c2989bd6dff9f447ef422ffd
coob <koosanghwi@gmail.com>
2024-05-26 14:05:46 +0900

    ver 1.9722

--- data_rom/mymodule/dead.py
+++ data_rom/mymodule/dead.py
@@ -75,7 +75,7 @@ def dead_recover(cla):
 
         recover_start = False
 
-        for i in range(60):
+        for i in range(100):
             full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\experience_title.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
