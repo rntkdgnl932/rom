ee3ef69b8f4ff11d8ecfb8c4b213454971ebb3a6
coob <koosanghwi@gmail.com>
2024-05-27 11:34:30 +0900

    ver 1.9724

--- data_rom/mymodule/dead.py
+++ data_rom/mymodule/dead.py
@@ -28,6 +28,16 @@ def dead_check(cla):
         if imgs_ is not None and imgs_ != False:
             print("dead_title", imgs_.x, imgs_.y)
             recovery = True
+            for i in range(60):
+                full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\dead_confirm.PNG"
+                img_array = np.fromfile(full_path, np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                imgs_ = imgs_set_(300, 410, 600, 450, cla, img, 0.7)
+                if imgs_ is not None and imgs_ != False:
+                    print("dead_confirm", imgs_.x, imgs_.y)
+                    click_pos_reg(imgs_.x, imgs_.y, cla)
+                    break
+                time.sleep(1)
 
         else:
             full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\experience_title.PNG"
