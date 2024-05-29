a05f973098e04ffe6814498112538764a85c2d92
coob <koosanghwi@gmail.com>
2024-05-29 09:04:19 +0900

    ver 1.9741

--- data_rom/mymodule/dead.py
+++ data_rom/mymodule/dead.py
@@ -31,22 +31,22 @@ def dead_check(cla):
             print("dead_title", imgs_.x, imgs_.y)
             recovery = True
             for i in range(60):
-                full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\dead_confirm.PNG"
+                full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\0000.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(300, 410, 600, 450, cla, img, 0.7)
+                imgs_ = imgs_set_(380, 280, 450, 320, cla, img, 0.95)
                 if imgs_ is not None and imgs_ != False:
-                    print("dead_confirm", imgs_.x, imgs_.y)
-                    click_pos_reg(imgs_.x, imgs_.y, cla)
+                    recovery = False
+                    temporary_recovery = True
                     break
                 else:
-                    full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\0000.PNG"
+                    full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\dead_confirm.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(380, 280, 450, 320, cla, img, 0.95)
+                    imgs_ = imgs_set_(300, 410, 600, 450, cla, img, 0.7)
                     if imgs_ is not None and imgs_ != False:
-                        recovery = False
-                        temporary_recovery = True
+                        print("dead_confirm", imgs_.x, imgs_.y)
+                        click_pos_reg(imgs_.x, imgs_.y, cla)
                         break
                 time.sleep(1)
 
