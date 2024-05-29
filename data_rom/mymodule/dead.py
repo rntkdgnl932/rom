4bc6c05a04ec4732a1dd74ecf8cb027e5fbdb269
coob <koosanghwi@gmail.com>
2024-05-29 12:45:20 +0900

    ver 1.9742

--- data_rom/mymodule/dead.py
+++ data_rom/mymodule/dead.py
@@ -45,8 +45,28 @@ def dead_check(cla):
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                     imgs_ = imgs_set_(300, 410, 600, 450, cla, img, 0.7)
                     if imgs_ is not None and imgs_ != False:
-                        print("dead_confirm", imgs_.x, imgs_.y)
-                        click_pos_reg(imgs_.x, imgs_.y, cla)
+                        full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\0000.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(380, 280, 450, 320, cla, img, 0.95)
+                        if imgs_ is not None and imgs_ != False:
+                            recovery = False
+                            temporary_recovery = True
+                            full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\dead_confirm.PNG"
+                            img_array = np.fromfile(full_path, np.uint8)
+                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                            imgs_ = imgs_set_(300, 410, 600, 450, cla, img, 0.7)
+                            if imgs_ is not None and imgs_ != False:
+                                print("dead_confirm", imgs_.x, imgs_.y)
+                                click_pos_reg(imgs_.x, imgs_.y, cla)
+                        else:
+                            full_path = "c:\\my_games\\rom\\data_rom\\imgs\\dead\\dead_confirm.PNG"
+                            img_array = np.fromfile(full_path, np.uint8)
+                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                            imgs_ = imgs_set_(300, 410, 600, 450, cla, img, 0.7)
+                            if imgs_ is not None and imgs_ != False:
+                                print("dead_confirm", imgs_.x, imgs_.y)
+                                click_pos_reg(imgs_.x, imgs_.y, cla)
                         break
                 time.sleep(1)
 
